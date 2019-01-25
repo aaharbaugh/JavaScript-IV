07d4aeb5c6ff17595b63fc05f4ff17bf91bd4655
aaharbaugh <aaronmharbaugh@gmail.com>
2019-01-24 21:19:12 -0600

    completing lambdaschool class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,23 @@
 // CODE here for your Lambda Classes
+
+
+class Person {
+    constructor(attributes){
+        this.name = attributes.name
+        this.gender = attributes.gender
+        this.age = attributes.age
+        this.location = attributes.location
+    }
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}`
+    }
+}
+
+class Instructor extends Person{
+    constructor(attributes){
+        super(attributes)
+        this.specialty = attributes.specialty
+        this.favLanguage = attributes.favLanguage
+        this.catchPhrase = attributes.catchPhrase
+    }
+}
